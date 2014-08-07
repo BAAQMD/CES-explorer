adc2851195f05bbdcbd8a1cc5b81e7ff0f07ce41
David Holstius <david.holstius@gmail.com>
2014-08-07 13:19:11 -0700

    Initial import

---
+++ server.R
@@ -0,0 +1,269 @@
+suppressPackageStartupMessages({
+  library(shinyIncubator)
+  library(dplyr)
+  library(tidyr)
+  library(CalEnviroScreen)
+  library(leaflet)
+  library(RColorBrewer)
+})
+
+data(CES2, package = "CalEnviroScreen")
+data(CES2_metadata, package = "CalEnviroScreen")
+data(California, package = "CalEnviroScreen")
+
+CES2_VARS <- c(CES2_POLLUTION_VARS, CES2_POPCHAR_VARS)
+
+REGION_COLORS <- c(`Bay Area` = "#009E73", `South Coast` = "#0072B2",
+                   `San Joaquin` = "#D55E00", `Other` = "#999999")
+
+region_tbl <- do.call(rbind, lapply(names(CA_regions), function (x) as.tbl(data.frame(FIPS = as.character(CA_regions[[x]]$FIPS), Region = x))))
+with_region <- function (.data) .data %>% inner_join(region_tbl, by = "FIPS")
+
+library(rgeos)
+tract_boundaries <- geometry(CA_tracts)
+
+#theme_set(theme_bw())
+#theme_update(
+#  plot.title = element_text(size=rel(1), face="bold", vjust=1.75),
+#  axis.title.x = element_text(size=rel(0.9), lineheight=1.1, face="bold", vjust=-0.5),
+#  axis.title.y = element_text(size=rel(0.9), lineheight=1.1, face="bold", angle=90)
+#)
+
+options(digits=3)
+
+scale_color_regions <- function (...) scale_color_manual("Region", values=region_colors)
+scale_fill_regions <- function (...) scale_fill_manual("Region", values=region_colors)
+scale_x_score <- function (...) scale_x_continuous(..., limits=c(0, 10), expand=c(0, 0))
+scale_y_score <- function (...) scale_y_continuous(..., limits=c(0, 10), expand=c(0, 0))
+
+region_colors <- c(`Bay Area`="#009E73", `South Coast`="#0072B2", `San Joaquin`="#D55E00", `Other`="#999999")
+
+region_tbl <- do.call(rbind, lapply(names(CA_regions), function (x) as.tbl(data.frame(FIPS = as.character(CA_regions[[x]]$FIPS), Region = x))))
+
+with_region <- function (.data) {
+  .data %>% inner_join(region_tbl, by = "FIPS")
+}
+
+xapply <- function (...) {
+  unlist(sapply(..., simplify = FALSE))
+}
+
+coord_getter <- function (j) {
+  f <- function (x) c(x@coords[, j, drop=TRUE], NA)
+  function (obj) xapply(obj@Polygons, f)
+}
+
+###############################################################################
+# Define server logic
+###############################################################################
+
+shinyServer(function(input, output, session) {
+
+  output$map <- reactive(TRUE)
+
+  map <- createLeafletMap(session, "map")
+
+  color_ramp <- function (x, pal = "RdYlGn") {
+    palette <- colorRampPalette(rev(brewer.pal(9, pal)))
+    palette(n = 1 + length(levels(x)))[x]
+  }
+
+  # session$onFlushed is necessary to work around a bug in the Shiny/Leaflet
+  # integration; without it, the addCircle commands arrive in the browser
+  # before the map is created.
+  session$onFlushed(once=TRUE, function() {
+    paintObs <- observe({
+
+      scores <- .impacted_scores()
+
+      map$clearShapes()
+      features <- CA_tracts[scores$FIPS,]
+
+      # Bug in Shiny causes this to error out when user closes browser
+      # before we get here
+      try({
+
+        xs <- xapply(features@polygons, coord_getter(1))
+        ys <- xapply(features@polygons, coord_getter(2))
+        xy_range <- as.vector(t(bbox(features)))
+        ids <- xapply(features@polygons, function (obj) rep(obj@ID, length(obj@Polygons)))
+        multipoly <- list(x = xs, y = ys, range = xy_range, names = ids)
+
+        multipoly$colors <- color_ramp(scores$Range)
+        class(multipoly) <- "map"
+
+        map$addPolygon(multipoly$y, multipoly$x, layerId = seq_along(multipoly$names),
+                       options = lapply(multipoly$colors, function(x) list(fillColor = x)),
+                       list(fill=TRUE, fillOpacity=0.5, stroke=TRUE, opacity=1, color="#000000", weight=0.1))
+
+      })
+      #}
+    })
+
+    # TIL this is necessary in order to prevent the observer from
+    # attempting to write to the websocket after the session is gone.
+    session$onSessionEnded(paintObs$suspend)
+  })
+
+  .region_boundary <- reactive({
+    region <- CA_regions[[input$region_name]]
+    region$boundary
+  })
+
+  .region_FIPS <- reactive({
+    #subset(CA_tracts, Region == input$region_name)
+    CA_regions[[input$region_name]]$FIPS
+  })
+
+  .selected_variables <- reactive({
+    c(input$pollution_vars, input$popchar_vars)
+  })
+
+  .pollution_weights <- reactive({
+    c(Ozone=input$Ozone, PM25=input$PM25, DieselPM=input$DieselPM, DrinkWat=input$DrinkWat, PestUse=input$PestUse, ToxRel=input$ToxRel, Traffic=input$Traffic,
+      Cleanup=input$Cleanup, GndWat=input$GndWat, HazWaste=input$HazWaste, ImpWat=input$ImpWat, SolWaste=input$SolWaste)
+  })
+
+  .popchar_weights <- reactive({
+    c(Age=input$Age, Asthma=input$Asthma, LBW=input$LBW, Edu=input$Edu, LingIso=input$LingIso, Poverty=input$Poverty, Unemp=input$Unemp)
+  })
+
+  .group_tbl <- reactive({
+    as.tbl(data.frame(Variable = CES2_VARS)) %>%
+      mutate(Group = factor(ifelse(Variable %in% CES2_POPCHAR_VARS, "PopChar", "Pollution")))
+  })
+
+  .weight_tbl <- reactive({
+    w <- c(.pollution_weights(), .popchar_weights())
+    as.tbl(data.frame(Variable = names(w), Weight = w))
+  })
+
+  .meta_tbl <- reactive({
+    inner_join(.group_tbl(), .weight_tbl(), by = "Variable")
+  })
+
+  .pctl_tbl <- reactive({
+    CES2_data %>% mutate_each(funs(pctl), -FIPS)
+  })
+
+  .subscore_tbl <- reactive({
+    if (length(.selected_variables()) == 0) {
+      as.tbl(data.frame(FIPS=character(0), Pollution=numeric(0), PopChar=numeric(0)))
+    } else {
+      min_obs <- Reduce(min, c(4, length(input$pollution_vars), length(input$popchar_vars)))
+    .pctl_tbl() %>%
+      gather(Variable, Pctl, -FIPS) %>%
+      inner_join(.meta_tbl(), by = "Variable") %>%
+      filter(Variable %in% .selected_variables()) %>%
+      group_by(FIPS, Group) %>%
+      compute_CES2_subscores(min_obs = min_obs) %>%
+      spread(Group, Subscore) %>%
+      arrange(desc(FIPS))
+    }
+  })
+
+  .score_tbl <- reactive({
+    if (input$method == "CES 2.0") {
+      subscores <- .subscore_tbl()
+
+      if (is.null(subscores$PopChar)) {
+        subscores$PopChar <- 1
+      } else {
+        if (is.null(subscores$Pollution))
+          subscores$Pollution <- 1
+      }
+      scores <- subscores %>% compute_CES2_scores()
+    } else {
+      cut_quantile <- function (x, ...) {
+        q <- quantile(x, seq(0, 1, len=21))
+        cut(x, breaks = q, labels = names(q)[-1])
+      }
+      summarise_rank_product <- function (.data) {
+        .data %>%
+          filter(!is.na(Value)) %>%
+          group_by(Variable) %>%
+          mutate(Rank = rank(-Value) + 1, Frac = Rank / n()) %>%
+          ungroup() %>%
+          group_by(FIPS) %>%
+          summarise(Score = sum(-log(Frac))) %>%
+          mutate(Percentile = 100 * normalize(rank(Score)),
+                 Range = cut_quantile(Score, n=20))
+      }
+      scores <- CES2_data %>%
+        gather(Variable, Value, -FIPS) %>%
+        filter(Variable %in% .selected_variables()) %>%
+        summarise_rank_product()
+    }
+    scores %>% arrange(desc(Score)) %>% with_region()
+  })
+
+  .impacted_percentile <- reactive({
+    100 - extract_numeric(input$impacted_percentile) # reverse the scale
+  })
+
+  .pollution_maximum <- reactive(with(.score_tbl(), max(Pollution, na.rm=TRUE)))
+  .popchar_maximum <- reactive(with(.score_tbl(), max(PopChar, na.rm=TRUE)))
+  .score_cutoff <- reactive(with(.score_tbl(), quantile(Score, .impacted_percentile() / 100, na.rm=TRUE)))
+  .popchar_intercept <- reactive(.score_cutoff() / .pollution_maximum())
+
+  .tally <- reactive({
+    .score_tbl() %>%
+      group_by(Region) %>%
+      dplyr::summarise(Tracts=n(), Yes=sum(Percentile > .impacted_percentile()), No=Tracts-Yes)
+  })
+
+  .scatterplot <- reactive({
+    fig_tbl <- .score_tbl() %>% mutate(Sampled = as.logical(rbinom(n(), 1, prob = input$SampleTracts / 100)))
+    cutoff_function <- function (x) {
+      ifelse(x < .popchar_intercept() | x > .popchar_maximum(), NA, .score_cutoff() / x)
+    }
+    fig <- ggplot(fig_tbl, aes(PopChar, Pollution)) +
+      coord_equal() + scale_x_score("Population Characteristics\n") + scale_y_score("Pollution Burden") +
+      scale_color_regions() + scale_fill_regions() +
+      geom_abline(slope=1, intercept=0, alpha=0.15) +
+      geom_point(aes(color=Region), alpha=I(0.3), size=I(2), subset=.(Sampled)) +
+      scale_alpha(range=c(0.3, 0.6)) +
+      stat_function(fun=cutoff_function, geom="line", linetype="dashed", alpha=0.7) +
+      annotate("text", x=.popchar_intercept(), y=.pollution_maximum(), label=str_c("Top ", 100 - .impacted_percentile(), "%"), hjust=1.1, vjust=1.1) +
+      guides(alpha=FALSE, color=guide_legend("Region", override.aes = list(alpha = 0.5)), fill=FALSE)
+    if (input$DensityPath) {
+      fig <- fig + stat_density2d(aes(color=Region, alpha=..level..), subset=.(Region != "Other"))
+    }
+    if (input$DensityFill) {
+      fig <- fig + stat_density2d(aes(fill=Region), alpha=I(0.03), color=NA, geom="polygon", subset=.(Region != "Other"))
+    }
+    fig <- fig + theme(legend.position="bottom", legend.direction="horizontal")
+    return(fig)
+  })
+
+  #.barchart <- reactive({
+  #  require(reshape2)
+  #  .tally() %>%
+  #    reshape2::melt(measure.vars = c("No", "Yes"), variable.name = "Impacted", value.name = "Freq") %>%
+  #    transform(Frac = Freq / Tracts) %>%
+  #    ggplot(aes(x=Region, y=Freq)) +
+  #    geom_bar(aes(fill=Impacted), stat="identity") +
+  #    scale_fill_manual(values=c(gray(0.7), gray(0.4))) +
+  #    scale_y_continuous(limits=c(0, 4500), expand=c(0, 0)) +
+  #    geom_text(aes(y=Tracts, label=str_c(percent(round(Yes/Tracts, 2)), " (n=", Yes, ")")), data=.tally(), vjust=-0.5) +
+  #    theme(legend.position="none", axis.title=element_blank())
+  #})
+
+  .impacted_scores <- reactive({
+    .score_tbl() %>% filter(Percentile > .impacted_percentile())
+  })
+
+  .impacted_FIPS <- reactive({
+    .impacted_scores()$FIPS
+  })
+
+  output$tally <- renderDataTable(.tally())
+  output$scatterplot <- renderPlot(show(.scatterplot()))
+  output$barchart <- renderPlot(show(.barchart()))
+
+  output$meta_tbl <- renderTable(.meta_tbl())
+  output$pctl_tbl <- renderDataTable(.pctl_tbl(), options = list(bSortClasses=TRUE, iDisplayLength=10))
+  output$data_tbl <- renderDataTable(inner_join(.pctl_tbl(), .score_tbl(), by = "FIPS"), options = list(bSortClasses=TRUE, iDisplayLength=10))
+
+  output$subscore_tbl <- renderDataTable(.subscore_tbl())
+})
---
+++ ui.R
@@ -0,0 +1,110 @@
+library(shinyIncubator)
+library(leaflet)
+library(CalEnviroScreen)
+
+data(CES2, package = "CalEnviroScreen")
+data(CES2_metadata, package = "CalEnviroScreen")
+
+weightInput <- function (inputId, label=paste0(inputId, ":"), value=1.0, ...) {
+  sliderInput(inputId, label, value=value, min=0, max=1, step=0.1, ...)
+}
+
+multiSelectInput <- function (inputId, label, choices, ...) {
+  selectInput(inputId, label, choices, selected=choices, multiple=TRUE, ...)
+}
+
+REGION_NAMES <- c("San Joaquin", "South Coast", "Bay Area", "Other")
+
+shinyUI(
+
+  navbarPage(
+    "CalEnviroScreen Explorer",
+    id = "nav",
+
+    tabPanel(
+      "Interactive map",
+      div(class="outer",
+
+          tags$head(
+            includeCSS("styles.css")#,
+            #includeScript("gomap.js")
+          ),
+
+          leafletMap(
+            "map", width="100%", height="100%",
+            initialTileLayer = "http://{s}.tiles.mapbox.com/v3/dholstius.j4a4ab9e/{z}/{x}/{y}.png",
+            initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
+            options = list(
+              center = c(36.5, -119.5),
+              zoom = 7,
+              maxBounds = list(list(17, -180), list(59, 180))
+            )
+          ),
+
+          absolutePanel(
+            id = "controls", class = "modal", fixed = TRUE, draggable = TRUE,
+            top = 60, left = "auto", right = 20, bottom = "auto",
+            width = 270, height = "auto",
+
+            h3("Model Settings"),
+
+            selectInput("method", "Methodology:", choices = c("CES 2.0", "Rank product"), selected = "CES 2.0"),
+            multiSelectInput("pollution_vars", "Pollution burden:", choices = unname(CES2_POLLUTION_VARS)),
+            multiSelectInput("popchar_vars", "Population characteristics:", choices = unname(CES2_POPCHAR_VARS)),
+            selectInput("impacted_percentile", "Impacted (% statewide):",
+                        choices = c( "5 %" =  5, "10 %" = 10, "15 %" = 15,
+                                     "20 %" = 20, "25 %" = 25, "30 %" = 30,
+                                     "35 %" = 35, "40 %" = 40, "45 %" = 45, "50 %" = 50),
+                        selected = 20)
+          ),
+
+          tags$div(
+            id="cite",
+            'Data: ', tags$a('CalEnviroScreen 2.0', href="http://oehha.ca.gov/ej/ces2.html"), '| Code: ', tags$a('Superzip', href="https://github.com/jcheng5/superzip")
+          )
+      )
+    ),
+
+    tabPanel(
+      "Data explorer",
+      dataTableOutput("data_tbl")
+    ),
+
+    tabPanel(
+      "Weights",
+      fluidRow(
+        column(3,
+               h4("Exposure"),
+               weightInput("Ozone"),
+               weightInput("PM25"),
+               weightInput("DieselPM"),
+               weightInput("DrinkWat"),
+               weightInput("PestUse"),
+               weightInput("ToxRel"),
+               weightInput("Traffic")),
+
+        column(3,
+               h4("Environment"),
+               weightInput("Cleanup",  value = 0.5),
+               weightInput("GndWat",   value = 0.5),
+               weightInput("HazWaste", value = 0.5),
+               weightInput("ImpWat",   value = 0.5),
+               weightInput("SolWaste", value = 0.5)),
+
+        column(3,
+               h4("Sensitivity"),
+               weightInput("Age"),
+               weightInput("Asthma"),
+               weightInput("LBW")),
+
+        column(3,
+               h4("Socioecon"),
+               weightInput("Edu"),
+               weightInput("LingIso"),
+               weightInput("Poverty"),
+               weightInput("Unemp")))
+    ),
+
+    conditionalPanel("false", icon("crosshair"))
+
+  ))
